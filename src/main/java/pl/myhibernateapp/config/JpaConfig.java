311f711696cd83045230e90491097a485fe7b28a
eMIKARp <emil.karpowicz@gmail.com>
2018-06-20 10:44:41 +0200

    Remove persistance.xml from app

--- src/main/java/pl/myhibernateapp/config/JpaConfig.java
+++ src/main/java/pl/myhibernateapp/config/JpaConfig.java
@@ -1,16 +1,43 @@
 package pl.myhibernateapp.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class JpaConfig {
 
 	@Bean
-	public LocalEntityManagerFactoryBean createEMF() {
-		LocalEntityManagerFactoryBean emf = new LocalEntityManagerFactoryBean();
+	public LocalContainerEntityManagerFactoryBean createEMF(JpaVendorAdapter adapter) {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		Map<String, String> properties = new HashMap<String, String>();
+        properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/library");
+        properties.put("javax.persistence.jdbc.user", "root");
+        properties.put("javax.persistence.jdbc.password", "Polska01");
+        properties.put("javax.persistence.jdbc.driver", "com.mysql.jdbc.Driver");
+        properties.put("javax.persistence.schema-generation.database.action", "drop-and-create");
 		emf.setPersistenceUnitName("myLibrary");
+		emf.setJpaPropertyMap(properties);
+		emf.setJpaVendorAdapter(adapter);
+		emf.setPackagesToScan("pl.myhibernateapp.model");
 		return emf;
 	}
+	
+	@Bean
+	
+	public JpaVendorAdapter createVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        return adapter;
+	}
+	
+	
 }
